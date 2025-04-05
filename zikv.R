94125d0272492439f0e1e2fe37efbc30d17a7c6d
Fathia Abdelkader <fathia.abdelkader4@gmail.com>
2025-04-05 06:03:46 -0500

    Describe what you changed here

--- zikv.R
+++ zikv.R
@@ -6,13 +6,11 @@ install.packages("devtools")
 
 install.packages("tibble")  
 
-install.
-
 library(tibble)
 
 # Download CIBERSORT.R and LM22.txt
 
-dir.create("~/Documents/cibersort", showWarnings = FALSE)
+dir.create("C:/Users/WA/Downloads/cibersort/CIBESORT.R", showWarnings = FALSE)
 
 # Download R script from a public GitHub repo
 
@@ -20,7 +18,7 @@ download.file(
   
   url = "https://raw.githubusercontent.com/JingYangSciBio/Immu-Mela/master/CIBERSORT.R",
   
-  destfile = "~/Documents/cibersort/CIBERSORT.R",
+  destfile = "C:/Users/WA/Downloads/cibersort/CIBERSORT.R",
   
   mode = "wb"
   
@@ -32,7 +30,7 @@ download.file(
   
   url = "https://raw.githubusercontent.com/mdozmorov/Immuno_notes/master/data/Cibersoft/LM22.txt",
   
-  destfile = "~/Documents/cibersort/LM22.txt",
+  destfile = "C:/Users/WA/Downloads/cibersort/LM22.txt",
   
   mode = "wb"
   
@@ -42,10 +40,8 @@ download.file(
 
 library(immunedeconv)
 
-set_cibersort_binary("~/Documents/cibersort/CIBERSORT.R")
-
-set_cibersort_mat("~/Documents/cibersort/LM22.txt")
-
+set_cibersort_binary("C:/Users/WA/Downloads/cibersort/CIBERSORT.R")
+set_cibersort_mat("C:/Users/WA/Downloads/cibersort/LM22.txt")
 
 
 # Inside R
@@ -78,12 +74,11 @@ library(dplyr)
 # List all .fpkm_tracking files
 files <- list.files("C:/Users/WA/Downloads/GSE87750_RAW/")
 files
-# Define a function to extract FPKM per sample
+############################Define a function to extract FPKM per sample#####################
 
 read_fpkm <- function(file) {
   
   dat <- read.delim(file)
-  
   sample_name <- tools::file_path_sans_ext(basename(file))
   
   sample_name <- sub(".fpkm_tracking", "", sample_name)
@@ -99,7 +94,7 @@ read_fpkm <- function(file) {
 }
 
 
-# Read and merge all samples into one matrix
+##################################Read and merge all samples into one matrix#######################
 
 expr_list <- lapply(files, read_fpkm)
 
@@ -111,57 +106,142 @@ expr_matrix <- Reduce(function(x, y) full_join(x, y, by = "gene"), expr_list) %>
   
   as.data.frame()
 
+
+
 rownames(expr_matrix) <- expr_matrix$gene
+
 
 expr_matrix$gene <- NULL
 
 head(rownames(expr_matrix), 10)
 
-# Map Ensembl IDs to HUGO symbols
 
-if (!requireNamespace("biomaRt", quietly = TRUE)) install.packages("biomaRt")
+##############################################Normalization########################################################
 
+install.packages("preprocessCore")
+
+library(preprocessCore)
+
+# Convert to matrix
+mat <- as.matrix(expr_matrix)
+
+# Quantile normalize
+mat_norm <- normalize.quantiles(mat)
+
+# Preserve row and column names
+rownames(mat_norm) <- rownames(expr_matrix)
+colnames(mat_norm) <- colnames(expr_matrix)
+
+# Convert back to data.frame if needed
+expr_matrix_normalized <- as.data.frame(mat_norm)
+
+# View a snippet
+head(expr_matrix_normalized[, 1:5])
+
+##################### comparison between the expression distribution before and after normalization
+boxplot(expr_matrix, 
+        las = 2, 
+        main = "Expression Distributions per Sample", 
+        outline = FALSE)
+
+
+boxplot(expr_matrix_normalized, 
+        las = 2, 
+        main = "Expression Distributions per Normalized Sample", 
+        outline = FALSE)
+############################################################ mapping #######################################3
+# Install biomaRt if you don't have it
+install.packages("biomaRt")
+
+# Load biomaRt library
 library(biomaRt)
 
-mart <- useEnsembl("ensembl", dataset = "hsapiens_gene_ensembl")
+# Connect to Ensembl database (use human genes)
+ensembl <- useMart("ensembl", dataset = "hsapiens_gene_ensembl")
 
-id_map <- getBM(
-  
-  attributes = c("ensembl_gene_id", "hgnc_symbol"),
-  
-  filters = "ensembl_gene_id",
-  
-  values = rownames(expr_matrix),
-  
-  mart = mart
-  
-)
+# Map Ensembl IDs to HUGO Gene Symbols (if your gene IDs are Ensembl IDs)
+gene_map <- getBM(attributes = c("ensembl_gene_id", "hgnc_symbol"),
+                  filters = "ensembl_gene_id", 
+                  values = rownames(expr_matrix_normalized),
+                  mart = ensembl)
 
-# Clean mapping
+# Merge the gene map with expr_matrix
+expr_matrix$gene_id <- rownames(expr_matrix)
 
-id_map <- id_map[id_map$hgnc_symbol != "", ]
+expr_matrix_with_symbols <- merge(expr_matrix, gene_map, by.x = "gene_id", by.y = "ensembl_gene_id", all.x = TRUE)
 
-id_map <- id_map[!duplicated(id_map$ensembl_gene_id), ]
 
-# Apply mapped symbols to matrix
 
-expr_matrix <- expr_matrix[rownames(expr_matrix) %in% id_map$ensembl_gene_id, ]
+rownames(expr_matrix_with_symbols) <- expr_matrix_with_symbols$gene_id
+expr_matrix_with_symbols$gene_id <- NULL  # Remove the temporary gene_id column
 
-rownames(expr_matrix) <- id_map$hgnc_symbol[match(rownames(expr_matrix), id_map$ensembl_gene_id)]
+expr_matrix <- expr_matrix_with_symbols
 
-# Filter to LM22 genes
+########################################################## lm22 quality check ####################
 
-lm22 <- read.delim("~/Documents/cibersort/LM22.txt", check.names = FALSE)
 
-lm22_genes <- rownames(lm22)
+# Reload original LM22 to get back the gene names
+lm22 <- read.delim("C:/Users/WA/Downloads/cibersort/LM22_fixed.txt", check.names = FALSE)
+
+# Optional: Rename first column to "GeneSymbol"
+colnames(lm22)[1] <- "GeneSymbol" 
+
+# Convert all other columns to numeric safely
+lm22[, -1] <- sapply(lm22[, -1], as.numeric)
+
+# Replace NA values with 0
+lm22[is.na(lm22)] <- 0
+
+# Sanity check – make sure GeneSymbol column still has text
+head(lm22$GeneSymbol)
+
+
+# Save it
+write.table(lm22,
+            file = "C:/Users/WA/Downloads/cibersort/LM22_fixed.txt",
+            sep = "\t",
+            row.names = FALSE,
+            quote = FALSE)
+
+########################################### Filter to LM22 genes ####################################
+lm22_genes <- lm22[1]
+
+sum(rownames(expr_matrix) %in% lm22_genes)
+
 
 expr_matrix_filtered <- expr_matrix[rownames(expr_matrix) %in% lm22_genes, ]
 
-# Run Cibersort
 
-res <- deconvolute(as.matrix(expr_matrix_filtered), method = "cibersort")
+# Ensure numeric matrix and clean formatting
+expr_matrix_filtered <- expr_matrix_filtered[complete.cases(expr_matrix_filtered), ]
+expr_matrix_filtered <- expr_matrix_filtered[!duplicated(rownames(expr_matrix_filtered)), ]
+expr_matrix_filtered <- as.matrix(expr_matrix_filtered)
+storage.mode(expr_matrix_filtered) <- "numeric"
 
-head(res)
+# Write filtered expression matrix to a text file for CIBERSORT
+write.table(expr_matrix_filtered,
+            file = "C:/Users/WA/Downloads/cibersort/expr_for_cibersort.txt",
+            sep = "\t",
+            row.names = FALSE,
+            quote = FALSE)
 
-This is what I have but I couldn't run the res <- deconvolute(as.matrix(expr_matrix_filtered), method = "cibersort") because it keeps giving me Error in if (dups > 0) { : argument is of length zero
- 
+######################################### Run CIBERSORT ####################################
+
+# Load the CIBERSORT R script
+source("C:/Users/WA/Downloads/cibersort/CIBERSORT.R")
+
+# Run CIBERSORT
+results <- CIBERSORT(
+  sig_matrix = "C:/Users/WA/Downloads/cibersort/LM22_fixed.txt",
+  mixture_file = "C:/Users/WA/Downloads/cibersort/expr_for_cibersort.txt",
+  perm = 100,
+  QN = FALSE  # for RNA-seq
+)
+
+# View results
+head(results)
+
+
+
+
+
